42318558684ab48986c161dfa1129bd9488d1345
Sayiddy <Sayidkamol2009@gmail.com>
2023-12-13 14:10:32 +0800

    tracks cart items and has loading message

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
+import { useRef, useState } from "react";
 import Product from "./component/product";
 import useFetch from "./component/useFetch";
 
 function App() {
+    const [cartOpen, setCartOpen] = useState(false)
+    const [cartItems, setCartItems] = useState([])
+
     const data = useFetch("http://localhost:3000/meals")
     console.log(data)
 
+    function onCart(name, price) {
+        setCartItems((cartItems)=>[...cartItems, {name: name, price: price}])
+    }
   return (
     <div>
         <header id="main-header">
             <h1 id="title">
-                <img src="./assets/logo.jpg"></img>
+                <img src="./assets"></img>
                 REACT FOOD
             </h1>
             <button className="text-button">cart</button>
         </header>
         <div id="meals">
-            {data && data.map((meal)=>{
-                <Product key={meal.id} imgUrl={meal.image} foodTitle={meal.name} price={meal.price} desc={meal.description}></Product>
+            {data ? data.map((meal)=>{
+                return <Product key={meal.id} imgUrl={meal.image} foodTitle={meal.name} price={meal.price} desc={meal.description} onCart={onCart}></Product>
             })
-            // :
-                // <div className="text-button">Loading Meals</div>
+            :
+                <div className="text-button">Loading Meals</div>
             }
         </div>
     </div>
--- src/component/Product.jsx
+++ src/component/Product.jsx
@@ -1,11 +1,11 @@
-export default function Product({imgUrl, foodTitle, price, desc}) {  
+export default function Product({imgUrl, foodTitle, price, desc, onCart}) {  
     return (
         <div className="meal-item">
             <img src={imgUrl} alt="food" />
             <h3>{foodTitle}</h3>
             <p className="meal-item-price">{"$" + price}</p>
             <p className="meal-item-description">{desc}</p>
-            <button className="meal-item-actions button">Add to Cart</button>
+            <button className="meal-item-actions button" onClick={()=>{onCart(foodTitle, price)}}>Add to Cart</button>
         </div>
     )
 };
